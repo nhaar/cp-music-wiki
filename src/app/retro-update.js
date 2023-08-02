5ec18d5ccc00d8fc62d8ea49eac366a6a4c42a51
nhaar <nhaar224131@gmail.com>
2023-08-02 17:41:14 -0300

    temporary definition of retro-update methods

--- src/app/retro-update.js
+++ src/app/retro-update.js
@@ -66,7 +66,7 @@ function addPath (obj, path) {
         obj[property] = []
       }
     } else {
-      if (nextObj) {
+      if (typeof nextObj === 'object') {
         iterator(nextObj, current + 1)
       } else {
         if (current < steps.length - 1) {
@@ -84,6 +84,123 @@ function addPath (obj, path) {
   return obj
 }
 
+function dropPath (obj, path) {
+  const steps = path.match(/\.\w+(\[\])*/g)
+
+  const iterator = (obj, current) => {
+    const step = steps[current]
+    const dimension = step.match(/(\[\])*/)[0].length
+    const property = step.replace(/\[\]|\./g, '')
+    const nextObj = obj[property]
+    console.log(step, current, steps.length - 1)
+
+    if (current < steps.length - 1) {
+      if (step.includes('[')) {
+        if (nextObj) {
+          const arrayiterator = (obj, i, dimension) => {
+            if (i < dimension) {
+              obj.forEach(element => {
+                arrayiterator(element, i + 1, dimension)
+              })
+            } else if (obj.length > 0) {
+              obj.forEach(element => {
+                iterator(element, current + 1)
+              })
+            }
+          }
+          arrayiterator(nextObj, 0, dimension)
+        }
+      } else {
+        if (nextObj) {
+          iterator(nextObj, current + 1)
+        }
+      }
+    } else {
+      delete obj[property]
+    }
+  }
+
+  iterator(obj, 0)
+
+  return obj
+}
+
+function objectifyPath (obj, path, propName) {
+  const steps = path.match(/\.\w+(\[\])*/g)
+
+  const iterator = (obj, current) => {
+    const step = steps[current]
+    const dimension = step.match(/(\[\])*/)[0].length
+    const property = step.replace(/\[\]|\./g, '')
+    const nextObj = obj[property]
+
+    if (step.includes('[')) {
+      // finish if the array doesn't exist, other iterate through everything
+      if (nextObj) {
+        const arrayiterator = (obj, i, dimension) => {
+          if (i < dimension) {
+            obj.forEach(element => {
+              arrayiterator(element, i + 1, dimension)
+            })
+          } else if (obj.length > 0) {
+            obj.forEach((element, i) => {
+              if (current < steps.length - 1) {
+                iterator(element, current + 1)
+              } else {
+                obj[i] = { [propName]: element }
+              }
+            })
+          }
+        }
+        arrayiterator(nextObj, 0, dimension)
+      } else {
+        obj[property] = []
+      }
+    } else {
+      if (typeof nextObj === 'object') {
+        iterator(nextObj, current + 1)
+      } else {
+        if (current < steps.length - 1) {
+          obj[property] = {}
+          iterator(obj[property], current + 1)
+        } else {
+          obj[property] = { [propName]: obj[property] }
+        }
+      }
+    }
+  }
+
+  iterator(obj, 0)
+
+  return obj
+}
+
+function readPath (obj, path) {
+  const steps = path.match(/\.\w+|\[.\]/g)
+  let output = obj
+
+  for (let i = 0; i < steps.length; i++) {
+    const step = steps[i].match(/(?<=\[).(?=\])|(?<=\.)\w+/)[0]
+    output = output[step]
+  }
+  return output
+}
+
+function assignPath (obj, path, value) {
+  const steps = path.match(/\.\w+|\[.\]/g)
+  let output = obj
+
+  for (let i = 0; i < steps.length; i++) {
+    const step = steps[i].match(/(?<=\[).(?=\])|(?<=\.)\w+/)[0]
+
+    if (i === steps.length - 1) {
+      output[step] = value
+    } else {
+      output = output[step]
+    }
+  }
+}
+
 async function addPathToAll (type, path) {
   const seq = await db.handler.getBiggestSerial(type)
   for (let i = 1; i <= seq; i++) {
@@ -92,5 +209,3 @@ async function addPathToAll (type, path) {
     overridePatches(type, i, versions)
   }
 }
-
-// addPathToAll('song', '.names[].lmao')
