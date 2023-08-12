702e5057ea6d7a542dfa2bd1d84c7388613978ad
nhaar <nhaar224131@gmail.com>
2023-08-12 10:17:02 -0300

    refact page system

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,17 +6,11 @@ const path = require('path')
 const apiRouter = require('./api')
 
 // homepage
-router.get('/', (req, res) => {
-  const content = `
-    <a href="lists"> Open lists </a>
-    <br>
-    <a href="pre-editor"> Go to the editor </a>
-  `
-  res.status(200).render('page.html', {
-    header: 'Main Page',
-    content
-  })
-})
+router.get('/', renderPage('Main Page', `
+  <a href="lists"> Open lists </a>
+  <br>
+  <a href="pre-editor"> Go to the editor </a>
+`))
 
 // editor selector
 router.get('/pre-editor', (req, res) => {
@@ -28,23 +22,21 @@ router.get('/editor', (req, res) => {
   res.status(200).render('editor.html')
 })
 
-router.get('/lists', (req, res) => {
-  const content = `
-  <a href="series-list"> Series OST </a>
-  `
-  res.status(200).render('page.html', {
-    header: 'OST Lists',
-    content
-  })
-})
+router.get('/lists', renderPage('OST Lists', `
+<a href="series-list"> Series OST </a>
+`))
 
 router.get('/series-list', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '../views/generated/series-ost.html'))
 })
 
-router.get('/user-page', (req, res) => {
-  res.status(200).render('user-page.html')
-})
+router.get('/user-page', renderPage('Login', `
+  <input class="name" placeholder="Name">
+  <input class="password" placeholder="Pass">
+  <button class="send">SEND</button>
+
+  <script src="../scripts/user-page.js" type="module"></script>
+`))
 
 // api
 router.use('/api', apiRouter)
@@ -53,5 +45,11 @@ router.use('/api', apiRouter)
 router.use('*', (req, res) => {
   res.status(404).send('Page not found')
 })
+
+function renderPage (header, content) {
+  return (req, res) => {
+    res.status(200).render('page.html', { header, content })
+  }
+}
 
 module.exports = router
