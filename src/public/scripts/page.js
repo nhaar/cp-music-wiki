efe9296fc20c471173f21301e4a6235c46ac1d8c
nhaar <nhaar224131@gmail.com>
2023-08-16 13:55:57 -0300

    add dynamic sidebar

--- src/public/scripts/page.js
+++ src/public/scripts/page.js
@@ -1,4 +1,4 @@
-import { getCookies, selectElement } from './utils.js'
+import { getCookies, selectElement, styleElement } from './utils.js'
 
 const cookies = getCookies()
 
@@ -7,3 +7,25 @@ if (cookies.session) {
   anchor.setAttribute('href', '/')
   anchor.innerHTML = cookies.username
 }
+
+const menu = selectElement('menu-img')
+const sidebarElement = selectElement('sidebar')
+
+let sidebar = cookies.sidebar !== 'true'
+function swapImage () {
+  sidebar = !sidebar
+  menu.src = sidebar
+    ? 'images/double-arrow.png'
+    : 'images/menu.png'
+  if (sidebar) {
+    document.cookie = 'sidebar=true'
+    sidebarElement.classList.remove('hidden')
+  } else {
+    styleElement(sidebarElement, 'hidden')
+    document.cookie = 'sidebar=false'
+  }
+}
+
+swapImage()
+
+menu.addEventListener('click', swapImage)
