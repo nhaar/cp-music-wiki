3e9916f8d3f0d65875e97f4644fdd4fbdc1dd3e8
nhaar <nhaar224131@gmail.com>
2023-08-18 14:54:00 -0300

    add css management in bundler

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
             loader: 'html-loader'
           }
         ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
