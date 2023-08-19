19cb1f8e63be15b1a2b06ffe5c091bc36db60247
nhaar <nhaar224131@gmail.com>
2023-08-19 07:58:39 -0300

    restore recent changes styles

--- src/client/scripts/components/RecentChanges.js
+++ src/client/scripts/components/RecentChanges.js
@@ -1,10 +1,86 @@
 import React from 'react'
 import { getJSON, getMonthName } from '../utils'
 import '../../stylesheets/recent-changes.css'
+import Gear from '../../images/gear.png'
+import Arrow from '../../images/arrow-down.png'
+
+function Settings (props) {
+  const className = `settings--options ${props.showSettings ? '' : 'hidden'}`
+
+  function clickResults (i) {
+    return () => props.settings.setResults(() => i)
+  }
+
+  function clickPeriod (i) {
+    return () => props.settings.setPeriod(() => i)
+  }
+
+  function createButton (value, i, click, state) {
+    const className = state === i ? 'selected-button' : ''
+    return <button className={className} key={i} onClick={click(i)}> {value} </button>
+  }
+
+  return (
+    <div className={className}>
+      <div className='settings--top settings--half'>
+        <div className='value-picker'>
+          <span className='bold'> Results to Show </span>
+          <div>
+            {props.settings.resultOptions.map((value, i) => createButton(value, i, clickResults, props.settings.results))}
+          </div>
+        </div>
+        <div className='settings--group'>
+          <input type='checkbox' />
+          <span> Group results by page</span>
+        </div>
+      </div>
+      <div className='settings--bottom settings--half'>
+        <span className='date-title bold'> Time period to search </span>
+        <div className='value-picker'>
+          <span className='gray bold'> Recent hours </span>
+          <div>
+            {props.settings.hourOptions.map((value, i) => createButton(value, i, clickPeriod, props.settings.period))}
+          </div>
+        </div>
+        <div className='value-picker'>
+          <span className='gray bold'> Recent days </span>
+          <div>
+            {props.settings.dayOptions.map((value, i) => createButton(value, i + props.settings.hourOptions.length, clickPeriod, props.settings.period))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function ChangesSetting () {
+  const [showSettings, setShowSettings] = React.useState(true)
+  function click () {
+    setShowSettings(prev => !prev)
+  }
+  const [results, setResults] = React.useState(0)
+  const [period, setPeriod] = React.useState(0)
+  const resultOptions = [50, 100, 250, 500]
+  const hourOptions = [1, 2, 6, 12]
+  const dayOptions = [1, 3, 7, 14, 30]
+
+  const settingVars = { results, setResults, period, setPeriod, resultOptions, hourOptions, dayOptions }
+
+  function addPlural (state, ...options) {
+    return !options.includes(state) ? 's' : ''
+  }
+
+  function pickPeriodWord () {
+    return period > 3 ? 'day' : 'hour'
+  }
+
   return (
-    <div />
+    <div className='settings--container' onClick={click}>
+      <img className='gear-img' src={Gear} />
+      <button className='settings--button'> {resultOptions[results]} change{addPlural(results, 0)}, {hourOptions.concat(dayOptions)[period]} {pickPeriodWord()}{addPlural(period, 0, 4)} </button>
+      <img className='arrow-img' src={Arrow} />
+      <Settings showSettings={showSettings} settings={settingVars} />
+    </div>
   )
 }
 
@@ -36,13 +112,13 @@ function Changes () {
           curDate = day
           const lastIndex = elements.length - 1
           const lastElement = elements[lastIndex]
-          if (lastElement && lastElement.type === 'span') {
+          if (lastElement && lastElement.type !== 'ul') {
             elements.splice(lastIndex, 1)
           }
           elements.push(
-            <span key={i * (-1)}>
+            <h4 key={i * (-1)}>
               {day}
-            </span>
+            </h4>
           )
         }
         if ((curDate !== day && curDate !== undefined) || i === change.length - 1) {
@@ -87,7 +163,7 @@ export default function RecentChanges () {
       <p>
         Track the most recent changes to the wiki on this page.
       </p>
-      <div>
+      <div className='settings--div'>
         <ChangesSetting />
       </div>
       <Changes />
