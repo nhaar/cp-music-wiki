c1797c12c595a5fbd9fa345db20af0646a595b70
nhaar <nhaar224131@gmail.com>
2023-08-20 21:09:45 -0300

    remove obsolete frontend code

--- src/client/scripts/utils.js
+++ src/client/scripts/utils.js
@@ -1,83 +1,4 @@
 /**
- * Selects element with a CSS class within another element or within the document
- * @param {string} className
- * @param {HTMLElement} element - Element to search in, default: document
- * @returns {HTMLElement}
- */
-export function selectElement (className, element = document) {
-  return element.querySelector('.' + className)
-}
-
-/**
- * Selects all elements with a CSS class within another element or within the document
- * @param {string} className
- * @param {HTMLElement} element - Element to search in, default: document
- * @returns {HTMLElement}
- */
-export function selectElements (className, element = document) {
-  return element.querySelectorAll('.' + className)
-}
-
-/**
- * Creates an element following the specified options
- * @param {object} options
- * @param {string} options.tag - Tag name, like 'input', if 'div', don't need to write it
- * @param {string} options.parent - If want to append to an element, its parent reference
- * @param {string} options.className - If want to give it a single CSS class
- * @param {string} options.innerHTML - To replace its innerHTML
- * @param {string[]} options.classes - Array with all classes to add to it
- * @param {string} options.type - If want to set a type, like 'number' for inputs
- * @param {string} options.value - If want to set a value
- * @param {object} options.dataset - Object where each key is a data variable name and its value
- * @param {boolean} options.checked - True if it's a checked checkbox
- * @returns {HTMLElement}
- */
-export function createElement (options) {
-  let tag
-  if (options) ({ tag } = options)
-  if (!tag) tag = 'div'
-
-  const newElement = document.createElement(tag)
-  if (options) {
-    const { parent, className, innerHTML, classes, type, value, dataset, checked } = options
-    let { tag } = options
-    if (!tag) tag = 'div'
-
-    if (className) newElement.className = className
-
-    if (classes) {
-      classes.forEach(className => {
-        newElement.classList.add(className)
-      })
-    }
-
-    if (innerHTML) newElement.innerHTML = innerHTML
-
-    if (type) newElement.setAttribute('type', type)
-
-    if (checked) newElement.setAttribute('checked', checked)
-
-    if (value) newElement.value = value
-
-    if (dataset) {
-      for (const variable in dataset) {
-        newElement.dataset[variable] = dataset[variable]
-      }
-    }
-
-    if (parent) parent.appendChild(newElement)
-  }
-
-  return newElement
-}
-
-export function styleElement (element, ...classes) {
-  classes.forEach(className => {
-    element.classList.add(className)
-  })
-}
-
-/**
  * Asynchronously posts with JSON as content
  * @param {string} route - Route to post to
  * @param {object} object - Object to convert to JSON
@@ -154,19 +75,6 @@ export function findIndexInObject (object, property, value) {
   return findElementInObject(object, property, value).i
 }
 
-/**
- * Gets the taken data for an input
- * @param {HTMLInputElement} element - Reference to the input
- * @param {string} variable - Name of data variable
- * @returns {import('./query-options.js').TakenInfo}
- */
-export function getTakenVariable (element, variable) {
-  const value = element.dataset[variable]
-  const hasUntakenId = !value
-  const takenIds = [value]
-  return { hasUntakenId, takenIds }
-}
-
 export function deepcopy (obj) { return JSON.parse(JSON.stringify(obj)) }
 
 export function getCookies () {
@@ -180,22 +88,6 @@ export function getCookies () {
   }
 
   return cookies
-}
-
-/**
- * Converts URL parameters into an object
- * containing the values of each of the query parameters
- * @param {URLSearchParams} urlParams - URL parameters to target
- * @returns {object} Object for the query parameters
- */
-export function paramsToObject () {
-  const urlParams = new URLSearchParams(window.location.search)
-  const params = {}
-  const paramsArray = [...urlParams.entries()]
-  paramsArray.forEach(array => {
-    params[array[0]] = array[1]
-  })
-  return params
 }
 
 export function getMonthName (month) {
