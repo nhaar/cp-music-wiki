01d552aec29d7aeff43e201f37b4c4750b3c0c87
nhaar <nhaar224131@gmail.com>
2023-08-27 10:50:16 -0300

    simplify util files

--- src/server/misc/common-utils.js
+++ src/server/misc/common-utils.js
@@ -1,20 +1,18 @@
-function getName (querywords) {
-  return querywords && querywords.match('^.*(&|$)')[0]
+module.exports = {
+  getName (querywords) {
+    return querywords && querywords.match('^.*(&|$)')[0]
+  },
+  formatCookies (str) {
+    const matches = str.match(/\w+=\w+(?=($|;))/g)
+    const cookies = {}
+    if (matches) {
+      matches.forEach(match => {
+        const words = match.match(/\w+/g)
+        cookies[words[0]] = words[1]
+      })
+    }
+
+    return cookies
+  },
+  MIN_PASSWORD_LENGTH: 8
 }
-
-function formatCookies (str) {
-  const matches = str.match(/\w+=\w+(?=($|;))/g)
-  const cookies = {}
-  if (matches) {
-    matches.forEach(match => {
-      const words = match.match(/\w+/g)
-      cookies[words[0]] = words[1]
-    })
-  }
-
-  return cookies
-}
-
-const MIN_PASSWORD_LENGTH = 8
-
-module.exports = { getName, formatCookies, MIN_PASSWORD_LENGTH }
--- src/server/misc/server-utils.js
+++ src/server/misc/server-utils.js
@@ -1,22 +1,5 @@
 const fs = require('fs')
 
-/**
- * Checks if two objects are the exact same (down to the order of things)
- * @param {object} obj1
- * @param {object} obj2
- * @returns {boolean}
- */
-function compareObjects (obj1, obj2) {
-  return JSON.stringify(obj1) === JSON.stringify(obj2)
-}
-
-/**
- * Get a deepcopy of an object
- * @param {object} object - Object to copy
- * @returns {object} Copied object
- */
-function deepcopy (object) { return JSON.parse(JSON.stringify(object)) }
-
 function groupPatterns (...patterns) {
   const sources = (patterns.map(pattern => {
     if (pattern instanceof RegExp) {
@@ -29,50 +12,69 @@ function groupPatterns (...patterns) {
   return new RegExp(combined)
 }
 
-function matchGroup (str, flags, ...patterns) {
-  return str.match(new RegExp(groupPatterns(...patterns), flags))
-}
+module.exports = {
+  /**
+   * Checks if two objects are the exact same (down to the order of things)
+   * @param {object} obj1
+   * @param {object} obj2
+   * @returns {boolean}
+   */
+  compareObjects (obj1, obj2) {
+    return JSON.stringify(obj1) === JSON.stringify(obj2)
+  },
 
-/**
- * Remove all curly brace characters from a string
- * @param {string} str
- * @returns {string}
- */
-function removeBraces (str) {
-  return str.replace(/{|}/g, '')
-}
+  /**
+   * Get a deepcopy of an object
+   * @param {object} object - Object to copy
+   * @returns {object} Copied object
+   */
+  deepcopy (object) { return JSON.parse(JSON.stringify(object)) },
 
-/**
- * Makes the first letter of a string uppercase
- * @param {string} str - String to modify
- * @returns Modified string
- */
-function capitalize (str) {
-  return `${str[0].toUpperCase()}${str.slice(1)}`
-}
+  groupPatterns,
 
-function createDirectoryIfNotExists (directoryPath) {
-  if (!fs.existsSync(directoryPath)) {
-    fs.mkdirSync(directoryPath)
+  matchGroup (str, flags, ...patterns) {
+    return str.match(new RegExp(groupPatterns(...patterns), flags))
+  },
+
+  /**
+   * Remove all curly brace characters from a string
+   * @param {string} str
+   * @returns {string}
+   */
+  removeBraces (str) {
+    return str.replace(/{|}/g, '')
+  },
+
+  /**
+   * Makes the first letter of a string uppercase
+   * @param {string} str - String to modify
+   * @returns Modified string
+   */
+  capitalize (str) {
+    return `${str[0].toUpperCase()}${str.slice(1)}`
+  },
+
+  createDirectoryIfNotExists (directoryPath) {
+    if (!fs.existsSync(directoryPath)) {
+      fs.mkdirSync(directoryPath)
+    }
+  },
+
+  /**
+   * Match for a pattern than enclosures everything inside two characters
+   * @param {string} str - String to match
+   * @param {string} lChar - Left character of the enclosure
+   * @param {string} rChar - Right character of the enclosure, leave blank for same as left
+   * @returns {object | null} Match result
+   */
+  matchInside (str, lChar, rChar) {
+    if (!rChar) rChar = lChar
+    return str.match(`(?<=${lChar}).*(?=${rChar})`)
+  },
+
+  getToken (req) {
+    const { cookie } = req.headers
+    const match = cookie.match(/(?<=(session=))[\d\w]+(?=(;|$))/)
+    return match && match[0]
   }
 }
-
-/**
- * Match for a pattern than enclosures everything inside two characters
- * @param {string} str - String to match
- * @param {string} lChar - Left character of the enclosure
- * @param {string} rChar - Right character of the enclosure, leave blank for same as left
- * @returns {object | null} Match result
- */
-function matchInside (str, lChar, rChar) {
-  if (!rChar) rChar = lChar
-  return str.match(`(?<=${lChar}).*(?=${rChar})`)
-}
-
-function getToken (req) {
-  const { cookie } = req.headers
-  const match = cookie.match(/(?<=(session=))[\d\w]+(?=(;|$))/)
-  return match && match[0]
-}
-
-module.exports = { compareObjects, deepcopy, groupPatterns, matchGroup, removeBraces, capitalize, createDirectoryIfNotExists, matchInside, getToken }
