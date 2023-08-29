732f046ff238296b34346eb657a564f189db12fa
nhaar <nhaar224131@gmail.com>
2023-08-29 08:05:19 -0300

    add first version of grouped changes

--- src/client/scripts/components/RecentChanges.jsx
+++ src/client/scripts/components/RecentChanges.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getCookies, getMonthName, postAndGetJSON } from '../client-utils'
 import '../../stylesheets/recent-changes.css'
 import Gear from '../../images/gear.png'
@@ -20,6 +20,11 @@ function Settings (props) {
     return <button className={className} key={i} onClick={click(i)}> {value} </button>
   }
 
+  function handleCheckbox (e) {
+    console.log(e.target.checked)
+    props.settings.setGroupTogether(e.target.checked)
+  }
+
   return (
     <div className={className}>
       <div className='settings--top settings--half'>
@@ -30,7 +35,7 @@ function Settings (props) {
           </div>
         </div>
         <div className='settings--group'>
-          <input type='checkbox' />
+          <input type='checkbox' checked={props.settings.groupTogether} onChange={handleCheckbox} />
           <span> Group results by page</span>
         </div>
       </div>
@@ -77,8 +82,197 @@ function ChangesSetting (props) {
   )
 }
 
+function GroupedChange (props) {
+  const [expanded, setExpanded] = useState(false)
+  const userCount = {}
+  props.info.users.forEach(user => {
+    if (userCount[user]) userCount[user]++
+    else userCount[user] = 1
+  })
+
+  return (
+    <div
+      style={{
+        marginBottom: '20px'
+      }}
+    >
+      <div>
+        <div
+          onClick={() => setExpanded(!expanded)} style={{
+            cursor: 'pointer',
+            userSelect: 'none',
+            display: 'inline-block'
+          }}
+        >
+          {'>'}&nbsp;&nbsp;&nbsp;
+        </div>
+        {props.info.time}&nbsp;
+        <a
+          style={{
+            fontWeight: 'bold'
+          }} href={`/Special:Read?id=${props.info.id}`}
+        >{props.item}
+        </a> &#40;{props.info.changes.length} changes | history&#41; . .&nbsp;
+        <span className={getDeltaClass(props.info.size)}>{props.info.size}&#41;</span> . .
+        &#91;
+        {Object.entries(userCount).map(userCount => {
+          const count = userCount[1]
+          return `${userCount[0]}${count === 1 ? '' : ` (${count}x)`}`
+        }).join(', ')}&#93;
+      </div>
+      {expanded
+        ? (
+          <div style={{
+            paddingLeft: '50px'
+          }}
+          >
+            {props.info.changes.map(getSingleLine)}
+          </div>
+          )
+        : undefined}
+    </div>
+  )
+}
+
+function GroupedChanges (props) {
+  const groupedData = {}
+  for (const day in props.data) {
+    const changes = props.data[day]
+    groupedData[day] = {}
+    changes.forEach(change => {
+      const key = `${change.cls} | ${change.name}`
+      if (groupedData[day][key]) {
+        groupedData[day][key].users.push(change.user)
+        groupedData[day][key].changes.push(change)
+        groupedData[day][key].size += change.delta
+      } else {
+        groupedData[day][key] = {
+          users: [change.user],
+          time: getTime(change.timestamp),
+          changes: [change],
+          size: change.delta,
+          id: change.id
+        }
+      }
+    })
+  }
+
+  const components = []
+  let i = 0
+  for (const day in groupedData) {
+    components.push(
+      <h4 key={i}>
+        {day}
+      </h4>
+    )
+    i++
+    const lis = []
+    const items = groupedData[day]
+    for (const item in items) {
+      const info = items[item]
+      lis.push(
+        <GroupedChange {...{ info, item }} key={i} />
+      )
+      i++
+    }
+    i++
+    components.push(
+      <div key={i}>
+        {lis}
+      </div>
+    )
+  }
+  return (
+    <div>
+      {components}
+    </div>
+  )
+}
+
+function UngroupedChanges (props) {
+  const elements = []
+  let i = 0
+  for (const day in props.data) {
+    const changes = props.data[day]
+    elements.push(
+      <h4 key={i}>{day}</h4>
+    )
+    i++
+    elements.push(
+      <ul key={i}>
+        {changes.map(getSingleLine)}
+      </ul>
+    )
+    i++
+  }
+  return (
+    <div>
+      {elements}
+    </div>
+  )
+}
+
+function getTime (timestamp) {
+  const date = new Date(Number(timestamp))
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+  return `${hours}:${minutes}`
+}
+
+function getDeltaClass (size) {
+  if (size > 0) {
+    return 'positive-diff'
+  }
+  if (size < 0) {
+    return 'negative-diff'
+  }
+  return 'zero-diff'
+}
+
+function getSingleLine (change, i) {
+  const time = getTime(change.timestamp)
+
+  const deltaClass = getDeltaClass(change.delta)
+
+  return (
+    <li key={`-${i}`}>
+      &#40;{change.old
+      ? (
+        <a href={`/Special:Diff?old=${change.old}&cur=${change.cur}`}> diff </a>
+        )
+      : ' diff '} | hist &#41;
+      . .
+      {change.old
+        ? undefined
+        : (
+          <span>
+            &nbsp;
+            <span
+              style={{
+                fontWeight: 'bold',
+                textDecoration: 'underline dotted',
+                cursor: 'help'
+              }} title='This edit created a new item'
+            >N
+            </span>
+            &nbsp;
+          </span>
+          )}
+      <a
+        href={`/Special:Read?id=${change.id}`} style={{
+          fontWeight: 'bold'
+        }}
+      >
+        {change.cls} | {change.name}
+      </a>; {time}
+      . . <span className={`${deltaClass} diff-number`}>{change.delta}</span> . . {change.user}
+
+    </li>
+  )
+}
+
 function Changes (props) {
-  const [elements, setElement] = React.useState([])
+  const [data, setData] = useState({})
 
   React.useEffect(() => {
     (async () => {
@@ -88,114 +282,49 @@ function Changes (props) {
           : props.HOUR_OPTIONS[props.period] / 24,
         number: props.RESULT_OPTIONS[props.results]
       })
-      const elements = []
 
-      let curDate
-      let currentList = []
-      data.forEach((change, i) => {
+      const dividedInDays = {}
+      data.forEach(change => {
         const date = new Date(Number(change.timestamp))
-        const hours = date.getHours().toString().padStart(2, '0')
-        const minutes = date.getMinutes().toString().padStart(2, '0')
-        const time = `${hours}:${minutes}`
         const day = `${date.getDate()} ${getMonthName(date.getMonth())} ${date.getFullYear()}`
-        let deltaClass
-        if (change.delta > 0) {
-          deltaClass = 'positive-diff'
-        } else if (change.delta < 0) {
-          deltaClass = 'negative-diff'
+        if (dividedInDays[day]) {
+          dividedInDays[day].push(change)
         } else {
-          deltaClass = 'zero-diff'
-        }
-
-        currentList.push(
-          <li key={`-${i}`}>
-            &#40;{change.old
-            ? (
-              <a href={`/Special:Diff?old=${change.old}&cur=${change.cur}`}> diff </a>
-              )
-            : ' diff '} | hist &#41;
-            . .
-            {change.old
-              ? undefined
-              : (
-                <span>
-                  &nbsp;
-                  <span
-                    style={{
-                      fontWeight: 'bold',
-                      textDecoration: 'underline dotted',
-                      cursor: 'help'
-                    }} title='This edit created a new item'
-                  >N
-                  </span>
-                  &nbsp;
-                </span>
-                )}
-            <a
-              href={`/Special:Read?id=${change.id}`} style={{
-                fontWeight: 'bold'
-              }}
-            >
-              {change.cls} | {change.name}
-            </a>; {time}
-            . . <span className={`${deltaClass} diff-number`}>{change.delta}</span> . . {change.user}
-
-          </li>
-        )
-
-        // no ul means remove datwe
-        function setDate (day) {
-          curDate = day
-          const lastIndex = elements.length - 1
-          const lastElement = elements[lastIndex]
-          if (lastElement && lastElement.type !== 'ul') {
-            elements.splice(lastIndex, 1)
-          }
-          elements.push(
-            <h4 key={`+${i}`}>
-              {day}
-            </h4>
-          )
-        }
-        if ((curDate !== day && curDate !== undefined) || i === data.length - 1) {
-          setDate(day)
-          elements.push(
-            <ul key={i}>
-              {currentList}
-            </ul>
-          )
-          currentList = []
-        } else if (curDate === undefined) {
-          setDate(day)
+          dividedInDays[day] = [change]
         }
       })
-
-      setElement(elements)
+      setData(dividedInDays)
     })()
   }, [props.results, props.period])
 
-  return (
-    <div>
-      {elements}
-    </div>
-  )
+  return props.groupTogether
+    ? (
+      <GroupedChanges data={data} />
+      )
+    : (
+      <UngroupedChanges data={data} />
+      )
 }
 
 export default function RecentChanges () {
   const cookies = getCookies()
-  const [results, setResults] = React.useState(Number(cookies.recentChangeResults) || 0)
-  const [period, setPeriod] = React.useState(Number(cookies.recentChangePeriod) || 0)
+  const [results, setResults] = useState(Number(cookies.recentChangeResults) || 0)
+  const [period, setPeriod] = useState(Number(cookies.recentChangePeriod) || 0)
+  const [groupTogether, setGroupTogether] = useState(Number(cookies.recentChangeGroup) !== 0)
 
   useEffect(() => {
     document.cookie = `recentChangeResults=${results}`
     document.cookie = `recentChangePeriod=${period}`
-  }, [results, period])
+    document.cookie = `recentChangeGroup=${Number(groupTogether)}`
+  }, [results, period, groupTogether])
 
   const props = {
     results,
     period,
+    groupTogether,
     setResults,
     setPeriod,
+    setGroupTogether,
     RESULT_OPTIONS: [50, 100, 250, 500],
     HOUR_OPTIONS: [1, 2, 6, 12],
     DAY_OPTIONS: [1, 3, 7, 14, 30],
