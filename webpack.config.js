8fe821c450d0f2fe948019b2597a8cc497320eff
nhaar <nhaar224131@gmail.com>
2023-08-29 15:55:08 -0300

    refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,69 +2,93 @@ const path = require('path')
 const webpack = require('webpack')
 const exec = require('child_process').exec
 
-function getEntry (...names) {
-  const entry = {}
-  names.forEach(name => {
-    entry[name] = ['webpack-hot-middleware/client?reload=true', path.join(__dirname, `src/client/scripts/auto/${name}.js`)]
-  })
-  return entry
-}
+/**
+ * Class that handles the creation of the `module.exports` required for webpack to function
+ */
+class WebpackSetup {
+  /**
+   * @param  {...string} names - The list of the names of the javascript files that will entry points for a bundle
+   */
+  constructor (...names) {
+    Object.assign(this, { names })
 
-function getModule (...names) {
-  return {
-    mode: 'development',
-    entry: getEntry(...names),
-    module: {
-      rules: [
-        {
-          test: /(\.js$)|(\.jsx$)/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader'
-          }
-        },
-        {
-          test: /\.html$/,
-          use: [
-            {
-              loader: 'html-loader'
-            }
-          ]
-        },
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader']
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource'
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-      path: path.join(__dirname, 'src/client/dist'),
-      filename: '[name].bundle.js',
-      publicPath: '/'
-    },
-    devtool: 'inline-source-map',
-    plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      {
-        apply: compiler => {
-          compiler.hooks.afterEmit.tap('AfterEmitPlugin', () => {
-            exec('npx standard --fix', (err, stdout, stderr) => {
-              if (err) console.error(err)
-              if (stdout) process.stdout.write(stdout)
-              if (stderr) process.stderr.write(stderr)
-            })
+    /** This plugin is used to make sure the linter runs after each update of the hot module replacement plugin */
+    this.lintPlugin = {
+      apply: compiler => {
+        compiler.hooks.afterEmit.tap('AfterEmitPlugin', () => {
+          exec('npx standard --fix', (err, stdout, stderr) => {
+            if (err) console.error(err)
+            if (stdout) process.stdout.write(stdout)
+            if (stderr) process.stderr.write(stderr)
           })
-        }
+        })
       }
-    ]
+    }
+  }
+
+  /**
+   * Get the entry property for all of the entry point javascript files
+   * @returns {object} The entry property object
+   */
+  getEntry () {
+    const entry = {}
+    this.names.forEach(name => {
+      entry[name] = ['webpack-hot-middleware/client?reload=true', path.join(__dirname, `src/client/scripts/auto/${name}.js`)]
+    })
+    return entry
+  }
+
+  /**
+   * Get the `module.exports` for the webpack configuration
+   * @returns {object} The object for the exports
+   */
+  getExports () {
+    // apart from a few, most of this is just the standard webpack configuration
+    return {
+      mode: 'development',
+      entry: this.getEntry(),
+      module: {
+        rules: [
+          {
+            test: /(\.js$)|(\.jsx$)/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader'
+            }
+          },
+          {
+            test: /\.html$/,
+            use: [
+              {
+                loader: 'html-loader'
+              }
+            ]
+          },
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader']
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource'
+          }
+        ]
+      },
+      resolve: {
+        extensions: ['*', '.js', '.jsx']
+      },
+      output: {
+        path: path.join(__dirname, 'src/client/dist'),
+        filename: '[name].bundle.js',
+        publicPath: '/'
+      },
+      devtool: 'inline-source-map',
+      plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        this.lintPlugin
+      ]
+    }
   }
 }
 
-module.exports = getModule(...require('./src/server/auto/hashed-list'))
+module.exports = (new WebpackSetup(...require('./src/server/auto/hashed-list'))).getExports()
