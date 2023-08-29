5f68abd046e2ed890756ce635b1d41d317be28b4
nhaar <nhaar224131@gmail.com>
2023-08-29 18:49:39 -0300

    reorganize server

--- src/server/server.js
+++ src/server/server.js
@@ -1,30 +1,28 @@
-const express = require('express')
-const app = express()
-const webpack = require('webpack')
-const webpackDevMiddleware = require('webpack-dev-middleware')
-
 const path = require('path')
 
-const { createDirectoryIfNotExists } = require('./misc/server-utils');
+// express setup
+const express = require('express')
+const app = express()
 
+// webpack setup
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const config = require('../../webpack.config')
+const compiler = webpack(config);
+
+// initialize directories
 [
   '../client/music',
   '../client/scripts/auto'
-].forEach(dir => createDirectoryIfNotExists(path.join(__dirname, dir)))
+].forEach(dir => require('./misc/server-utils').createDirectoryIfNotExists(path.join(__dirname, dir)))
 
-require('./auto/generate-auto')
-
-const config = require('../../webpack.config')
-const compiler = webpack(config)
-
-const { PORT } = require('../../config')
-
-const SERVER_PORT = PORT
-
-const clsys = require('../server/database/class-system')
-
+// all requests are handled with JSON
 app.use(express.json())
 
+// generate html files
+require('./auto/generate-auto')
+
+// setup static/public files
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')))
 } else {
@@ -32,18 +30,20 @@ if (process.env.NODE_ENV === 'production') {
     webpackDevMiddleware(compiler, {
       publicPath: config.output.publicPath,
       stats: 'minimal'
-    })
-  )
+    }))
+  // `hot` enables auto-refresh, but it causes load issues if you switch pages too much
   if (process.env.NODE_ENV === 'hot') {
     app.use(require('webpack-hot-middleware')(compiler))
   }
 }
 
 // make sure tables are created before running router
-clsys.createTables().then(() => {
+require('../server/database/class-system').createTables().then(() => {
   app.use('/', require('./routes/index'))
 })
 
+// run on custom or environment port
+const SERVER_PORT = process.env.PORT || require('../../config').PORT
 app.listen(SERVER_PORT, () => {
   console.log(`Listening on port ${SERVER_PORT}`)
 })
