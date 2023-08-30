d91876a4e64fff7d844902be17c0d96a17ec4f25
nhaar <nhaar224131@gmail.com>
2023-08-30 16:52:19 -0300

    add docs

--- src/server/misc/email.js
+++ src/server/misc/email.js
@@ -1,12 +1,20 @@
 const nodemailer = require('nodemailer')
 const { EMAIL } = require('../../../config')
 
+/** Class that handles sending emails */
 class EmailSender {
-  constructor () {
-    this.transporter = nodemailer.createTransport(EMAIL)
-    this.origin = EMAIL.auth.user
-  }
+  /** `nodemailer` object initiated with the data given inside the `config` file */
+  transporter = nodemailer.createTransport(EMAIL)
 
+  /** Email address that will be used to send emails */
+  origin = EMAIL.auth.user
+
+  /**
+   * Send an email
+   * @param {string} recipient - Email address receiving the email
+   * @param {string} subject - Subject of the email
+   * @param {string} text - Email text body
+   */
   async sendEmail (recipient, subject, text) {
     await this.transporter.sendMail({
       from: this.origin,
