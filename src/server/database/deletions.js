b34fb9f97a84bf3e67f0a696fd321f631bd05236
nhaar <nhaar224131@gmail.com>
2023-09-01 20:45:28 -0300

    add docs

--- src/server/database/deletions.js
+++ src/server/database/deletions.js
@@ -2,18 +2,22 @@ const clsys = require('./class-system')
 const sql = require('./sql-handler')
 const user = require('./user')
 
+/** Class that handles deletions of items */
 class DeletionHandler {
+  /** Create tables if they don't exist */
   constructor () {
+    // table to save items that are deleted
     sql.create(`
-    deleted_items (
-      id SERIAL PRIMARY KEY,
-      cls TEXT,
-      item_id INT,
-      data JSONB,
-      querywords TEXT
-    )
-  `)
+      deleted_items (
+        id SERIAL PRIMARY KEY,
+        cls TEXT,
+        item_id INT,
+        data JSONB,
+        querywords TEXT
+      )
+    `)
 
+    // table to keep track of deletions
     sql.create(`
     deletion_log (
       id SERIAL PRIMARY KEY,
@@ -29,6 +33,13 @@ class DeletionHandler {
   `)
   }
 
+  /**
+   * Delete an item from the database
+   * @param {number} id - Item id
+   * @param {string} token - Session token of the user performing deletion
+   * @param {number} reason - Identifier for reason
+   * @param {string} otherReason - String with other reason
+   */
   async deleteItem (id, token, reason, otherReason) {
     const row = await clsys.getItem(id)
     sql.delete('items', 'id', id)
@@ -36,6 +47,12 @@ class DeletionHandler {
     this.insertDeletion(row, token, reason, otherReason, true)
   }
 
+  /**
+   * Bring a deleted item back to normal status
+   * @param {number} id - Item id
+   * @param {string} reason - Reason for undeleting
+   * @param {string} token - Session token for the user performing undeletion
+   */
   async undeleteItem (id, reason, token) {
     const row = await this.getDeletedRow(id)
     sql.delete('deleted_items', 'id', row.id)
@@ -43,6 +60,14 @@ class DeletionHandler {
     this.insertDeletion(id, token, null, reason, false)
   }
 
+  /**
+   * Add a deletion or undeletion into the deletion log
+   * @param {ItemRow} row - Row to delete/undelete
+   * @param {string} token - Session token for the user that performed the deletion/undeletion
+   * @param {number} reason - Reason id
+   * @param {string} other - Other reason text
+   * @param {boolean} isDeletion - `true` if this log entry pertains to a deletion, `false` if it pertains to an undeletion
+   */
   async insertDeletion (row, token, reason, other, isDeletion) {
     await sql.insert(
       'deletion_log',
@@ -51,6 +76,12 @@ class DeletionHandler {
     )
   }
 
+  /**
+   * Get all deleted items filtering the name by a string
+   * @param {string} cls - Item class
+   * @param {string} keyword - Filtering string
+   * @returns {object[]} List of all found items
+   */
   async getByName (cls, keyword) {
     const rows = await sql.selectLike('deleted_items', 'querywords', keyword, 'cls', cls)
     return clsys.getNameWithRows(rows.map(row => {
@@ -58,10 +89,20 @@ class DeletionHandler {
     }), keyword)
   }
 
+  /**
+   * Get the class of an item, including deleted and not deleted items
+   * @param {number} id - Item id
+   * @returns {string} Item class name
+   */
   async getClass (id) {
     return (await this.getItemIncludeDeleted(id)).cls
   }
 
+  /**
+   * Get the first name of an item, including deleted and not deleted items
+   * @param {number} id - Item id
+   * @returns {string} Found name, or empty string if no item was found
+   */
   async getQueryNameById (id) {
     try {
       return (await this.getItemIncludeDeleted(id)).querywords.split('&&')[0]
@@ -70,8 +111,13 @@ class DeletionHandler {
     }
   }
 
+  /**
+   * Get the row for a deleted item
+   * @param {number} id - Item id
+   * @returns {object} Row from the `deleted_items` table
+   */
   async getDeletedRow (id) {
-    return (await sql.selectWithColumn('deleted_items', 'item_id', id))[0]
+    return await sql.selectRowWithColumn('deleted_items', 'item_id', id)
   }
 
   async getItemIncludeDeleted (id) {
@@ -83,6 +129,11 @@ class DeletionHandler {
     return row
   }
 
+  /**
+   * Check if an item is deleted or not
+   * @param {number} id - Item id
+   * @returns {boolean} `true` if item is deleted, `false` if not
+   */
   async isDeleted (id) {
     return Boolean(await this.getDeletedRow(id))
   }
--- src/server/database/predefined.js
+++ src/server/database/predefined.js
@@ -1,9 +1,14 @@
+/** Class that represents a predefined item */
 class Predefined {
+  /** Construct object */
   constructor (cls, data, id) {
     Object.assign(this, { cls, data, id })
   }
 }
 
+/**
+ * List of all the predefined items in the database
+ */
 module.exports = [
   new Predefined('category', { name: 'OST List' }, 1)
 ]
